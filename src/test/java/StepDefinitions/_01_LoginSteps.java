d273c369cebaac970ff077cb1032675337c6afc1
TechnoStudy <ismettechno@gmail.com>
2023-09-12 20:05:18 +0300

    cucumber quite option

--- src/test/java/StepDefinitions/_01_LoginSteps.java
+++ src/test/java/StepDefinitions/_01_LoginSteps.java
@@ -6,17 +6,17 @@ public class _01_LoginSteps {
 
     @Given("Navigate to Campus")
     public void navigate_to_campus() {
-
+        System.out.println("site açıldı");
     }
 
     @When("Enter username and password and click login button")
     public void enter_username_and_password_and_click_login_button() {
-
+        System.out.println("username ve password girildi");
     }
 
     @Then("User should login successfully")
     public void user_should_login_successfully() {
-
+        System.out.println("girildiği test edildi");
     }
 
 }
