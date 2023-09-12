3cb1e668ffafc89a791d53bb37f42a4f81812b9e
TechnoStudy <ismettechno@gmail.com>
2023-09-12 19:32:46 +0300

    Login 1

---
+++ src/test/java/StepDefinitions/_01_LoginSteps.java
@@ -0,0 +1,22 @@
+package StepDefinitions;
+
+import io.cucumber.java.en.*;
+
+public class _01_LoginSteps {
+
+    @Given("Navigate to Campus")
+    public void navigate_to_campus() {
+
+    }
+
+    @When("Enter username and password and click login button")
+    public void enter_username_and_password_and_click_login_button() {
+
+    }
+
+    @Then("User should login successfully")
+    public void user_should_login_successfully() {
+
+    }
+
+}
