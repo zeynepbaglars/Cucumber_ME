788dc72b94b91b57e756853b83cdd4e9baa862cb
TechnoStudy <ismettechno@gmail.com>
2023-09-18 19:35:55 +0100

    DataTable Ornek son

--- src/test/java/StepDefinitions/_08_DataTableOrnek.java
+++ src/test/java/StepDefinitions/_08_DataTableOrnek.java
@@ -19,9 +19,21 @@ public class _08_DataTableOrnek {
 
     @And("Write username as DataTable")
     public void writeUsernameAsDataTable(DataTable userlar) {
-        List<String> listUserlar= userlar.asList(String.class);
+        List<String> listUserlar= userlar.asList(String.class); // liste
 
         for (String user : listUserlar)
             System.out.println("user = " + user);
+    }
+
+    @And("Write username and Password as DataTable")
+    public void writeUsernameAndPasswordAsDataTable(DataTable kullaniciVeSifreler) {
+       List< List<String> >  listKullaniciVeSifreler= kullaniciVeSifreler.asLists(String.class); // listelerin listesi
+
+        for (int i = 0; i < listKullaniciVeSifreler.size(); i++) {
+            System.out.println("ListItemleri = "+ listKullaniciVeSifreler.get(i).get(0)+" "+ // username
+                                                  listKullaniciVeSifreler.get(i).get(1) );   // password
+        }
+
+
     }
 }
