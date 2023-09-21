e177892b98f649be42b658ba575abbeacd6e4c1f
TechnoStudy <ismettechno@gmail.com>
2023-09-21 19:21:49 +0100

    ExtendReport İşlemleri 10 ek 1

--- src/test/java/Runners/_07_TestRunnerExtentReport.java
+++ src/test/java/Runners/_07_TestRunnerExtentReport.java
@@ -1,7 +1,9 @@
 package Runners;
 
+import com.aventstack.extentreports.service.ExtentService;
 import io.cucumber.testng.AbstractTestNGCucumberTests;
 import io.cucumber.testng.CucumberOptions;
+import org.testng.annotations.AfterClass;
 
 @CucumberOptions(
         tags = "@Regression",
@@ -10,5 +12,11 @@ import io.cucumber.testng.CucumberOptions;
         plugin = {"com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:"}
 )
 public class _07_TestRunnerExtentReport extends AbstractTestNGCucumberTests {
+
+    @AfterClass
+    public  static void writeExtentReport(){
+        ExtentService.getInstance().setSystemInfo("",""); // username : İsmet Temur
+
+    }
 
 }
