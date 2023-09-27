91c22c42e56999d35a1f9658a69ffacba3c8792e
TechnoStudy <ismettechno@gmail.com>
2023-09-27 18:00:43 +0100

    _07_Soru1

---
+++ src/test/java/ApachePOI/_07_Soru1.java
@@ -0,0 +1,15 @@
+package ApachePOI;
+
+/**
+ *  Ekrandaki gibi bir Excel Çarpım tablosunu
+ *  varolan boş bir excel dosyasına
+ *  1 den 10 kadar çarpımlar için yapınız.
+ *  (ipucu önce sadece 1 leri yapınız sonra
+ *  diğerlerini)
+ */
+
+public class _07_Soru1 {
+    public static void main(String[] args) {
+
+    }
+}
