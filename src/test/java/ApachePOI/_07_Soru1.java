de4169d392cc2d7d9bbf1fcce45f96a102058435
TechnoStudy <ismettechno@gmail.com>
2023-09-27 19:21:24 +0100

    _07_Soru1 çözüm

--- src/test/java/ApachePOI/_07_Soru1.java
+++ src/test/java/ApachePOI/_07_Soru1.java
@@ -1,5 +1,11 @@
 package ApachePOI;
 
+import org.apache.poi.ss.usermodel.*;
+
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
 /**
  *  Ekrandaki gibi bir Excel Çarpım tablosunu
  *  varolan boş bir excel dosyasına
@@ -9,7 +15,42 @@ package ApachePOI;
  */
 
 public class _07_Soru1 {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
 
+        String path = "src/test/java/ApachePOI/resource/CarpimTablosu.xlsx";
+        FileInputStream inputStream=new FileInputStream(path);
+        Workbook workbook= WorkbookFactory.create(inputStream);
+        Sheet sheet=workbook.getSheetAt(0);
+
+
+
+        int yeniAcilacakSatirIndexi= 0; //sheet.getPhysicalNumberOfRows();
+
+        for (int k = 1; k <= 10; k++) {
+
+            for (int i = 1; i <= 10; i++) {
+
+                // ++ her bır ıslemı yenı satıra yaz demek
+                Row satir = sheet.createRow(yeniAcilacakSatirIndexi++);
+
+                Cell hucre1 = satir.createCell(0); hucre1.setCellValue(k);
+                Cell hucre2 = satir.createCell(1); hucre2.setCellValue("x");
+                Cell hucre3 = satir.createCell(2); hucre3.setCellValue(i);
+                Cell hucre4 = satir.createCell(3); hucre4.setCellValue("=");
+                Cell hucre5 = satir.createCell(4); hucre5.setCellValue(k * i);
+            }
+
+            Row bosSatir = sheet.createRow(yeniAcilacakSatirIndexi++);
+        }
+
+        inputStream.close();
+
+        FileOutputStream outputStream=new FileOutputStream(path);
+        workbook.write(outputStream);
+        workbook.close();
+        outputStream.close();
+
+        System.out.println("işlem tamamlandı");
     }
+
 }
