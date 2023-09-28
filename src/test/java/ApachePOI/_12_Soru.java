659978b621f26d1ca5593aabd09c5ca9bb31d160
TechnoStudy <ismettechno@gmail.com>
2023-09-28 19:07:09 +0100

    Soru 12  çözüm

--- src/test/java/ApachePOI/_12_Soru.java
+++ src/test/java/ApachePOI/_12_Soru.java
@@ -9,22 +9,78 @@ package ApachePOI;
    dosya varsa en son satırın altına yaz.
  */
 
+import org.apache.poi.ss.usermodel.*;
+import org.apache.poi.xssf.usermodel.XSSFSheet;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+
 public class _12_Soru {
     public static void main(String[] args) {
-        path="src/test/java/ApachePOI/resource/SenaryoSonuclari.xlsx";
-        String bilgi="test passed";
+        String path = "src/test/java/ApachePOI/resource/SenaryoSonuclari2.xlsx";
+        String bilgi = "test passed";
 
-        exceleYaz(path,bilgi)
+        writeToExcel(path, bilgi); // excel create
+        writeToExcel(path, bilgi); // dosya artık var alt taraf çalışacak
+        writeToExcel(path, bilgi);
+        writeToExcel(path, bilgi);
     }
 
-    exceleYaz(path, bilgi){
+    public static void writeToExcel(String path, String bilgi) {
 
-        File file=new File(path);
+        File file = new File(path);
 
-        if (!file.exists) // dosya yok ise
+        if (!file.exists()) // dosya yok ise
+        {
+            //hafzada worbook oluştur, içinde hafızada sheet oluştur
+            XSSFWorkbook workbook = new XSSFWorkbook();
+            XSSFSheet sheet = workbook.createSheet("Sayfa1");
 
-        dosya yok ise oluştur ilk satıra verilen bilgiyi yaz
-        var ise , son satıra biliyi yaz
+            //hafızada işlemlerini yap
+            Row yeniSatir = sheet.createRow(0);
+            Cell hucre = yeniSatir.createCell(0);
+            hucre.setCellValue(bilgi+" create oldu");
+
+            //kaydet
+            try {
+                FileOutputStream outputStream = new FileOutputStream(path);
+                workbook.write(outputStream);
+                workbook.close();
+                outputStream.close();
+            } catch (Exception ex) {
+                System.out.println("ex.getMessage() = " + ex.getMessage());
+            }
+        } else {
+
+            FileInputStream inputStream=null;
+            Workbook workbook=null;
+            Sheet sheet=null;
+
+            try {
+                inputStream = new FileInputStream(path);
+                workbook = WorkbookFactory.create(inputStream);
+                sheet = workbook.getSheetAt(0);
+            } catch (Exception ex) {
+                System.out.println("ex.getMessage() = " + ex.getMessage());
+            }
+
+            int sonSatirIndex = sheet.getPhysicalNumberOfRows();
+            Row yeniSatir = sheet.createRow(sonSatirIndex);
+            Cell yeniHucre = yeniSatir.createCell(0);
+            yeniHucre.setCellValue(bilgi);
+
+            try {
+                inputStream.close();
+                FileOutputStream outputStream = new FileOutputStream(path);
+                workbook.write(outputStream);
+                workbook.close();
+                outputStream.close();
+            } catch (Exception ex) {
+                System.out.println("ex.getMessage() = " + ex.getMessage());
+            }
+        }
 
     }
 
