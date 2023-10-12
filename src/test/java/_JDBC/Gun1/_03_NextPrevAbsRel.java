05c70d8ffeecd4a6bb8c255b50368ab1c00e2753
TechnoStudy <ismettechno@gmail.com>
2023-10-12 19:09:15 +0100

    _03_NextPrevAbsRel test2

--- src/test/java/_JDBC/Gun1/_03_NextPrevAbsRel.java
+++ src/test/java/_JDBC/Gun1/_03_NextPrevAbsRel.java
@@ -12,14 +12,28 @@ public class _03_NextPrevAbsRel extends JDBCParent {
     public void test1() throws SQLException {
         ResultSet rs=sorguEkrani.executeQuery("select * from language");
 
-        rs.next();
+        rs.next(); // bir adım ileri
         System.out.println("1.Satır="+ rs.getString(2)); // 2.Column index değil      -ing
 
-        rs.next();
+        rs.next();  // bir adım ileri
         System.out.println("2.Satır="+ rs.getString("name")); // 2.Column index değil - ital
 
-        rs.previous();
+        rs.previous(); // bir adım geri
         System.out.println("1.Satır="+ rs.getString(2)); // 2.Column index değil      -ing
+    }
+
+    @Test
+    public void test2() throws SQLException {
+        ResultSet rs=sorguEkrani.executeQuery("select * from film");
+
+        rs.absolute(10); // 10 satıra direk git
+        System.out.println("10.Satır title="+ rs.getString("title"));
+
+        rs.absolute(5); // 5 satıra direk git
+        System.out.println("5.Satır title="+ rs.getString("title"));
+
+        rs.relative(5); // en son nerdeysen, ordan itibaren 5 git.
+        System.out.println("10.Satır title="+ rs.getString("title"));
     }
 
 
