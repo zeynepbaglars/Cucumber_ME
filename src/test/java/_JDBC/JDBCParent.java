7fe7cf2554b7c90c558fc899362f838eaac23909
TechnoStudy <ismettechno@gmail.com>
2023-10-12 18:20:21 +0100

    JDBCParent

--- src/test/java/_JDBC/JDBCParent.java
+++ src/test/java/_JDBC/JDBCParent.java
@@ -1,4 +1,41 @@
 package _JDBC;
 
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
+
 public class JDBCParent {
+
+    public static Connection baglanti;
+    @BeforeClass
+    public void DBConnectionOpen()
+    {
+        String HostUrl = "jdbc:mysql://db-technostudy.ckr1jisflxpv.us-east-1.rds.amazonaws.com/sakila";
+        String username = "root";
+        String password = "'\"-LhCB'.%k[4S]z";
+
+        try {
+            baglanti = DriverManager.getConnection(HostUrl, username, password);
+            Statement sorguEkrani = baglanti.createStatement();
+        }
+        catch(Exception ex){
+            System.out.println("ex.getMessage() = " + ex.getMessage());
+        }
+    }
+
+    @AfterClass
+    public void DBConnectionClose()
+    {
+        try {
+            baglanti.close();
+        } catch (SQLException ex) {
+            System.out.println("ex.getMessage() = " + ex.getMessage());
+        }
+    }
+
+
 }
