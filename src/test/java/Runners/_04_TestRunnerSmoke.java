d3351629056c30989e2a67f861e0051bdc8048ed
TechnoStudy <ismettechno@gmail.com>
2023-11-07 16:27:31 +0000

    Jenkins ayarları yapıldı

--- src/test/java/Runners/_04_TestRunnerSmoke.java
+++ src/test/java/Runners/_04_TestRunnerSmoke.java
@@ -8,6 +8,8 @@ import io.cucumber.testng.CucumberOptions;
         tags = "@SmokeTest",
         features = {"src/test/java/FeatureFiles"},
         glue={"StepDefinitions"}
+        ,plugin= {"json:target/cucumber/cucumber.json"} // Jenkins için JSON report
+
 )
 public class _04_TestRunnerSmoke extends AbstractTestNGCucumberTests {
 }
